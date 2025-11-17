a72cd079416ca9d75e20eb3e9ab4ba0426ada4f8
Arnav <arnavuniyal594@gmail.com>
2025-11-17 06:18:52 +0530

    login page fixed

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -6,29 +6,36 @@ export default function LoginPage() {
     const navigate = useNavigate();
 
     return (
-        <div className="login-wrapper">
-            <div className="login-left-panel">
-                <h2 className="login-left-heading">Welcome Back!</h2>
-                <p className="login-left-text">
-                    Please login with your personal info.
-                </p>
-                <button className="btn-sign-up" onClick={() => navigate("/signup")}>
-                    Sign Up
-                </button>
-            </div>
+        <div className="login-container">
+            <div className="login-card">
+                <div className="login-left-panel">
+                    <h2 className="login-left-heading">Welcome Back!</h2>
+                    <p className="login-left-text">Please login with your personal info.</p>
+                    <button className="btn-sign-up" onClick={() => navigate("/signup")}>
+                        Sign Up
+                    </button>
+                </div>
 
-            <div className="login-right-panel">
-                <h2 className="login-right-heading">Login to Your Account</h2>
-                <div className="login-input-box">
-                    <input type="email" className="login-input-field" placeholder="Email" required />
+                <div className="login-right-panel">
+                    <h2 className="login-right-heading">Login to Your Account</h2>
+
+                    <div className="login-input-box">
+                        <input type="email" className="login-input-field" placeholder="Email" required />
+                    </div>
+
+                    <div className="login-input-box">
+                        <input type="password" className="login-input-field" placeholder="Password" required />
+                    </div>
+
+                    <button className="btn-login">Login</button>
+
+                    <p className="login-signup-link">
+                        Don't have an account?
+                        <span className="login-signup-anchor" onClick={() => navigate("/signup")}>
+                            Sign Up
+                        </span>
+                    </p>
                 </div>
-                <div className="login-input-box">
-                    <input type="password" className="login-input-field" placeholder="Password" required />
-                </div>
-                <button className="btn-login">Login</button>
-                <p className="login-signup-link">
-                    Don't have an account? <span className="login-signup-anchor" onClick={() => navigate("/signup")}>Sign Up</span>
-                </p>
             </div>
         </div>
     );
